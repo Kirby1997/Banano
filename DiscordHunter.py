c40152ceeb02bed200ba4930c7f05df0bebd69a8
Kirby1997 <jj@wilkinsons.me.uk>
2019-08-19 03:42:40 +0100

    Tool for identifying who is sending transactions to who

---
+++ DiscordHunter.py
@@ -0,0 +1,64 @@
+import asyncio
+import aiohttp
+import json
+
+API_URL = 'https://api-beta.banano.cc:443'
+UFW_API = 'https://bananobotapi.banano.cc/ufw/'
+
+
+async def node_get(payload):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(API_URL, json=payload, timeout=100) as resp:
+                jsonResp = await resp.json()
+                return jsonResp
+    except BaseException:
+        print(BaseException)
+        return None
+
+
+async def get_rep(account):
+    payload = {"action": "account_representative", "account": account}
+    response = await node_get(payload)
+    rep = response['representative']
+    return rep
+
+
+async def get_discord(account):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(UFW_API+account) as resp:
+                jsonResp = await resp.json()
+                return jsonResp['user_name']
+    except BaseException:
+        print(BaseException)
+        return None
+
+
+async def main():
+    source = input("Enter an address to see transaction totals for ")
+    tranType = input("Do you want \"receive\" or \"send\" transaction totals? ")
+    noTrans = input("How many transactions should be retrieved: ")
+    if noTrans == "":
+        noTrans = 1
+
+    payload = {"action": "account_history", "account": source, "count": noTrans}
+
+    response = await node_get(payload)
+    addresses = set([])
+    pairs = {}
+
+    for i in response['history']:
+        account = i['account']
+        if account not in addresses:
+            addresses.add(account)
+            if i['type'] == tranType:
+                rep = await get_rep(account)
+                if rep == 'ban_1tipbotgges3ss8pso6xf76gsyqnb69uwcxcyhouym67z7ofefy1jz7kepoy':
+                    user = await get_discord(account)
+                    rep = user
+                pairs[account] = rep
+
+    print(json.dumps(pairs, indent=4))
+
+asyncio.run(main())
