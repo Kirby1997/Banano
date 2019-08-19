17a7369a6537f66c41cffb3dd0adda3a8ba532ef
Kirby1997 <jj@wilkinsons.me.uk>
2019-08-19 20:07:38 +0100

    Tool for finding an account's intermediaries

---
+++ ExchangeFinder.py
@@ -0,0 +1,62 @@
+import asyncio
+import aiohttp
+import aiofiles
+import json
+
+API_URL = 'https://api-beta.banano.cc:443'
+
+
+async def json_get(payload):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(API_URL, json=payload, timeout=100) as resp:
+                jsonResp = await resp.json()
+                return jsonResp
+    except BaseException:
+        print(BaseException)
+        return None
+
+
+async def get_history(address, noTrans = 1):
+    payload = {"action": "account_history", "account": address, "count": noTrans}
+    response = await json_get(payload)
+    return response['history']
+
+
+async def read_file():
+    known_labels = {}
+    async with aiofiles.open('exchanges.txt') as f:
+        linenum = 0
+        async for line in f:
+            linenum += linenum
+            keypair = line.strip()
+            keypair = keypair.split(":")
+            try:
+                known_labels[keypair[0]] = keypair[1]
+            except:
+                print("inconsistency at line {} containing \"{}\" ".format(linenum, line))
+                print("Formatting should be address:label")
+    return known_labels
+
+
+async def main():
+    source = input("Enter an address to find exchange wallets for: ")
+    noTrans = input("How many transactions should be retrieved: ")
+    known_labels = await read_file()
+    history = await get_history(source, int(noTrans))
+    addresses = set([])
+    intermediaries = {}
+    for i in history:
+        if i['type'] == "send":
+            addresses.add(i['account'])
+
+    for i in addresses:
+        history = await get_history(i, 200)
+        for x in history:
+            if x['account'] in known_labels and i not in intermediaries:
+                intermediaries[i] = known_labels[x['account']]
+
+    print(json.dumps(intermediaries, indent=4))
+
+
+asyncio.run(main())
