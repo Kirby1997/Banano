49797469cbcd92266355e84c155178ec10ce1a16
Kirby1997 <jj@wilkinsons.me.uk>
2019-10-31 20:55:38 +0000

    Increased speed of exchange resolver

--- TotalTo.py
+++ TotalTo.py
@@ -34,12 +34,23 @@ async def get_discord(account):
         return ""
 
 
+async def get_inter(address, labels):
+    if totals[address][2] != "":
+        return ""
+    else:
+        history = await get_history(address, 1)
+        for transaction in history:
+            if transaction['account'] in labels and transaction['type'] == "send":
+                totals[address][3] = labels[transaction['account']]
+
+
 async def download_users():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://bananobotapi.banano.cc/users') as resp:
             with open("users.json", "wb") as f:
                 async for data in resp.content.iter_chunked(1024):
                     f.write(data)
+
 
 async def download_exchanges():
     async with aiohttp.ClientSession() as session:
@@ -65,9 +76,7 @@ async def read_exchanges():
     return known_labels
 
 
-async def main():
-    source = input("Enter an address to see transaction totals for: ")
-    noTrans = input("How many transactions should be retrieved (default 50): ")
+async def check_updates():
     skipdlusers = False
     skipdlexch = False
     if not os.path.exists("users.json"):
@@ -90,44 +99,48 @@ async def main():
             print("Downloading exchanges.txt...")
             await download_exchanges()
 
+
+# Dictionary to use an address as a key and store attributes in a list.
+totals = {}
+
+async def main():
+    source = input("Enter an address to see transaction totals for: ")
+    noTrans = input("How many transactions should be retrieved (default 50): ")
+    await check_updates()
     if noTrans == "":
         noTrans = 50
 
-    response = await get_history(source, noTrans)
-    totals = {}
+    print("Getting the last ", noTrans, " transactions...")
+    history = await get_history(source, noTrans)
 
 
-    print("Searching history of account")
+    print("Searching history of account...")
+    for transaction in history:
+        amount = int(transaction['amount']) / 10 ** 29
+        destination = transaction['account']
+        if destination not in totals:
+            # List is received, sent, Discord name, Exchange
+            totals[destination] = list([0, 0, "", ""])
 
-    for i in response:
-        amount = int(i['amount']) / 10 ** 29
-        destination = i['account']
-
-        if i['type'] == "receive":
-            if destination not in totals:
-                totals[destination] = list([0, 0, "", ""])
+        if transaction['type'] == "receive":
             totals[destination][0] = totals[destination][0] + int(amount)
 
-        if i['type'] == "send":
-            if destination not in totals:
-                totals[destination] = list([0, 0, "", ""])
+        if transaction['type'] == "send":
             totals[destination][1] = totals[destination][1] + int(amount)
 
+    print("Identifying Discord users...")
     coros = [get_discord(address) for address in totals]
     names = await asyncio.gather(*coros)
 
-    labels = await read_exchanges()
-
-    for address in totals:
-        if totals[address][2] == "":
-            history = await get_history(address, 1)
-            for x in history:
-                if x['account'] in labels and x['type'] == "send":
-                    totals[address][3] = labels[x['account']]
-
-
     for address, name in zip(totals, names):
         totals[address][2] = name
+
+    print("Loading in exchange list...")
+    labels = await read_exchanges()
+
+    print("Searching for intermediaries to exchanges and gambling sites...")
+    coros = [get_inter(address, labels) for address in totals]
+    await asyncio.gather(*coros)
 
     csv_columns = ['Address', 'Received', 'Sent', 'Discord', 'Exchange']
     csv_file = "totals.csv"
