a8124572cc3571aa414b1b55af8b9c98ee8a0a62
Kirby1997 <jj@wilkinsons.me.uk>
2019-11-02 14:20:46 +0000

    Added resolving for Twitter and Telegram

--- TotalTo.py
+++ TotalTo.py
@@ -31,6 +31,34 @@ async def get_discord(account, users):
             totals[account][2] = entry["user_name"]
 
 
+async def get_twitter(account, users):
+    for entry in users:
+        if entry[0] == account:
+            totals[account][3] = entry[1]
+
+
+async def get_telegram(account, users):
+    for entry in users:
+        if entry[0] == account:
+            totals[account][4] = entry[1]
+
+
+async def format_txt(file):
+    file = file.strip("((")
+    file = file.strip("))")
+    names = []
+    addresses = []
+    for account in file.split("), ("):
+        # print(account)
+        account = account.split(", ")
+        names.append(account[1].strip("\'") + " - " + account[0].strip("\'"))
+        addresses.append(account[2].strip("\'"))
+
+    zipped = zip(addresses, names)
+    zipped = set(zipped)
+    return zipped
+
+
 async def get_inter(address, labels):
     if totals[address][2] != "":
         return ""
@@ -42,9 +70,34 @@ async def get_inter(address, labels):
 
 
 async def download_users():
+    await download_discord()
+    await download_telegram()
+    await download_twitter()
+
+
+async def download_discord():
+    print("Downloading discord.json...")
     async with aiohttp.ClientSession() as session:
         async with session.get('https://bananobotapi.banano.cc/users') as resp:
-            with open("users.json", "wb") as f:
+            with open("discord.json", "wb") as f:
+                async for data in resp.content.iter_chunked(1024):
+                    f.write(data)
+
+
+async def download_twitter():
+    print("Downloading twitter.txt...")
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://ba.nanotipbot.com/users/twitter') as resp:
+            with open("twitter.txt", "wb") as f:
+                async for data in resp.content.iter_chunked(1024):
+                    f.write(data)
+
+
+async def download_telegram():
+    print("Downloading telegram.txt...")
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://ba.nanotipbot.com/users/telegram') as resp:
+            with open("telegram.txt", "wb") as f:
                 async for data in resp.content.iter_chunked(1024):
                     f.write(data)
 
@@ -76,8 +129,7 @@ async def read_exchanges():
 async def check_updates():
     skipdlusers = False
     skipdlexch = False
-    if not os.path.exists("users.json"):
-        print("Downloading users.json...")
+    if not os.path.exists("discord.json") or not os.path.exists("telegram.txt") or not os.path.exists("twitter.txt"):
         skipdlusers = True
         await download_users()
     if not os.path.exists("exchanges.txt"):
@@ -86,10 +138,10 @@ async def check_updates():
         await download_exchanges()
 
     if not skipdlusers:
-        update = input("Update Discord users file? \"yes\" or \"no\" (default no): ")
+        update = input("Update user lists? \"yes\" or \"no\" (default no): ")
         if update in ["yes", "Yes", "Y", "y"]:
-            print("Downloading users.json...")
             await download_users()
+
     if not skipdlexch:
         update = input("Update exchanges file? \"yes\" or \"no\" (default no): ")
         if update in ["yes", "Yes", "Y", "y"]:
@@ -116,8 +168,8 @@ async def main():
         amount = int(transaction['amount']) / 10 ** 29
         destination = transaction['account']
         if destination not in totals:
-            # List is received, sent, Discord name, Exchange
-            totals[destination] = [0, 0, "", ""]
+            # List is received, sent, Discord name, Twitter name, Telegram name, Exchange
+            totals[destination] = [0, 0, "", "", "", ""]
 
         if transaction['type'] == "receive":
             totals[destination][0] = totals[destination][0] + int(amount)
@@ -125,12 +177,30 @@ async def main():
         if transaction['type'] == "send":
             totals[destination][1] = totals[destination][1] + int(amount)
 
-    print("Reading users.json file...")
-    with open("users.json", "r") as f:
+    print("Reading discord.json file...")
+    with open("discord.json", "r") as f:
         users = json.load(f)
 
     print("Identifying Discord users...")
     coros = [get_discord(address, users) for address in totals]
+    await asyncio.gather(*coros)
+
+    print("Reading twitter.txt file...")
+    with open("twitter.txt", "r") as f:
+        twitter = f.read()
+        twitter = await format_txt(twitter)
+
+    print("Identifying Twitter users...")
+    coros = [get_twitter(address, twitter) for address in totals]
+    await asyncio.gather(*coros)
+
+    print("Reading telegram.txt file...")
+    with open("telegram.txt", "r", encoding="utf-8") as f:
+        telegram = f.read()
+        telegram = await format_txt(telegram)
+
+    print("Identifying Telegram users...")
+    coros = [get_telegram(address, telegram) for address in totals]
     await asyncio.gather(*coros)
 
     print("Loading in exchange list...")
@@ -140,7 +210,7 @@ async def main():
     coros = [get_inter(address, labels) for address in totals]
     await asyncio.gather(*coros)
 
-    csv_columns = ['Address', 'Received', 'Sent', 'Discord', 'Exchange']
+    csv_columns = ['Address', 'Received', 'Sent', 'Discord', 'Twitter', 'Telegram', 'Exchange']
     csv_file = "totals.csv"
     try:
         print("Writing totals to totals.csv")
@@ -148,7 +218,7 @@ async def main():
             writer = csv.writer(csvfile)
             writer.writerow(csv_columns)
             for address in totals:
-                writer.writerow([address, totals[address][0], totals[address][1], totals[address][2], totals[address][3]])
+                writer.writerow([address, totals[address][0], totals[address][1], totals[address][2], totals[address][3], totals[address][4], totals[address][5]])
     except IOError as e:
         print(e)
 
