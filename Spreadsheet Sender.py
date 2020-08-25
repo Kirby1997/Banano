be354cca2576fa8a9c4ef675c8a33ec34898f558
Kirby1997 <jj@wilkinsons.me.uk>
2020-08-25 19:55:37 +0100

    Added Pippin Support

--- Spreadsheet Sender.py
+++ Spreadsheet Sender.py
@@ -4,12 +4,18 @@ import asyncio
 import aiohttp
 
 API_URL = 'https://api-beta.banano.cc:443'
+PIPPIN = ""
 
 
-async def json_get(payload):
+async def json_get(payload, use_pippin=False):
+    if use_pippin and PIPPIN != "":
+        url = PIPPIN
+    else:
+        url = API_URL
+
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.post(API_URL, json=payload, timeout=100) as resp:
+            async with session.post(url, json=payload, timeout=100) as resp:
                 jsonResp = await resp.json()
                 return jsonResp
     except BaseException as e:
@@ -21,20 +27,21 @@ async def load_addresses():
     filename = input("Enter the csv payment list here: \n")
     try:
         data = pd.read_csv(filename)
-        print(data)
         addresses = list(data["addresses"])
-        print(addresses)
         valid_addresses = set()
-        print(3)
 
         for address in addresses:
             if Validations.get_banano_address(address) is not None:
                 valid_addresses.add(Validations.get_banano_address(address))
-        print(valid_addresses)
-        f = open("paid.txt", "r")
+        print("Total addresses: " + str(len(valid_addresses)))
+        try:
+            f = open("paid.txt", "r")
+        except:
+            open("paid.txt", "x")
+            f = open("paid.txt", "r")
         previous = set(f.read().splitlines())
         unpaid = valid_addresses.difference(previous)
-        print(len(unpaid), " - unpaid:")
+        print(len(unpaid), "unpaid:")
         print(unpaid)
         input("Press enter to continue")
         return unpaid
@@ -48,7 +55,7 @@ async def load_addresses():
 async def get_pay():
     pay = ""
     while not isinstance(pay, int):
-        pay = input("Enter how much each account is going to be paid here: \n")
+        pay = input("How many banano is each to be paid? \n")
         if pay == "0":
             print("Enter something greater than 0!")
             pay = ""
@@ -63,6 +70,7 @@ async def get_pay():
 
 
 async def get_wallet():
+    use_pippin = True
     has_wallet = input("Do you already have a wallet on the node? [y/n]")
     if has_wallet in ["yes", "Yes", "Y", "y"]:
         walletID = input("Please enter your walletID: \n")
@@ -75,14 +83,15 @@ async def get_wallet():
                 "action": "wallet_create",
                 "seed": seed
             }
-            response = await json_get(payload)
+            response = await json_get(payload, use_pippin)
+            print(response)
             walletID = response['wallet']
             print("WalletID-" + walletID)
             return walletID
     else:
         print("Generating a temporary wallet...\n")
         payload = {"action": "wallet_create"}
-        response = await json_get(payload)
+        response = await json_get(payload, use_pippin)
         try:
             walletID = response['wallet']
             print("WalletID-" + walletID)
@@ -91,6 +100,7 @@ async def get_wallet():
             print(response)
             return None
 
+
 async def saverun(walletID, depositAddress):
     print("Account and wallet ID are being save to lastrun.txt to minimise potential losses")
     file = open("lastrun.txt", "w")
@@ -98,13 +108,15 @@ async def saverun(walletID, depositAddress):
     file.write("\nAddress: " + depositAddress)
     file.close()
 
+
 async def account_create(walletID):
+    use_pippin = True
     payload = {
         "action": "account_create",
         "wallet": walletID,
         "index": "0"
         }
-    response = await json_get(payload)
+    response = await json_get(payload, use_pippin)
     try:
         depositAddress = response['account']
         await saverun(walletID, depositAddress)
@@ -134,6 +146,7 @@ async def enough_bans(paymentadd, paytotal):
 
 
 async def send_payment(walletID, paymentadd, address, pay):
+    use_pippin = True
     payload = {
         "action": "send",
         "wallet": walletID,
@@ -142,7 +155,7 @@ async def send_payment(walletID, paymentadd, address, pay):
         "amount": str(pay * 10 ** 29),
     }
     try:
-        response = await json_get(payload)
+        response = await json_get(payload, use_pippin)
         block = response['block']
         print("Return block: " + block)
     except Exception as e:
@@ -151,6 +164,7 @@ async def send_payment(walletID, paymentadd, address, pay):
 
 
 async def destroy_wallet(walletID):
+    use_pippin = True
     destroy = input("Do you want to destroy the wallet used? [y/n]")
     if destroy in ["yes", "Yes", "Y", "y"]:
         print("\nDestroying wallet...")
@@ -158,7 +172,7 @@ async def destroy_wallet(walletID):
             "action": "wallet_destroy",
             "wallet": walletID
         }
-        response = await json_get(payload)
+        response = await json_get(payload, use_pippin)
         if response['destroyed'] == "1":
             print("Wallet has been destroyed")
         elif response['error'] == "Wallet not found":
@@ -174,7 +188,7 @@ async def main():
         walletID = await get_wallet()
         paymentadd = await account_create(walletID)
         while not await enough_bans(paymentadd, len(addresses)*pay):
-            input("Please ensure " + str(paymentadd) + " has enough bans to pay " + str(len(addresses)*pay) + "\n")
+            input("Please ensure " + str(paymentadd) + " has enough bans to pay " + str(len(addresses)*pay) + "banano\n")
         total = len(addresses)
 
         f = open("paid.txt", "a+")
